576fd8355e1187ab136e317f16654af3cc532765
黄欣欣 <huangxinxin01@corp.netease.com>
2021-08-04 19:03:30 +0800

    add modulemap file

---
+++ EOSSDK.framework/Headers/EOSSDKUmbrellaHeader.h
@@ -0,0 +1,26 @@
+//
+//  EOSSDKUmbrellaHeader.h
+//  EOSSDKUmbrellaHeaderApi_IOS
+
+#ifndef EOSSDKUmbrellaHeader_h
+#define EOSSDKUmbrellaHeader_h
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double StaticLibraryModuleVersionNumber;
+FOUNDATION_EXPORT const unsigned char StaticLibraryModuleVersionString[];
+
+#import "eos_common.h"
+#import "eos_sdk.h"
+#import "eos_auth.h"
+#import "eos_connect.h"
+#import "eos_logging.h"
+#import "eos_init.h"
+#import "eos_userinfo.h"
+#import "eos_ios.h"
+
+#include <set>
+
+
+#endif /* EOSSDKUmbrellaHeader_h */
+
